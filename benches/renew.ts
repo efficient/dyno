7d05c255c9cfd26b6d304269f4ad04e48d0476c5
Sol Boucher <sboucher@cmu.edu>
2022-02-18 23:33:47 -0500

    benches/renew.ts: Script for benchmarking libgotcha libset reinitialization

---
+++ benches/renew.ts
@@ -0,0 +1,26 @@
+import { BenchmarkTimer, bench, runBenchmarks } from '../deno/test_util/std/testing/bench.ts';
+
+const gotcha = Deno.dlopen('libgotcha.so', {
+	libgotcha_group_limit: {
+		parameters: [],
+		result: 'u64',
+	},
+	libgotcha_group_renew: {
+		parameters: ['i64'],
+		result: 'void',
+	},
+});
+
+const count = gotcha.symbols.libgotcha_group_limit() as number;
+let iter = 0;
+bench({
+	name: 'renew',
+	func: function(timer: BenchmarkTimer) {
+		timer.start();
+		gotcha.symbols.libgotcha_group_renew(iter++ % count + 1);
+		timer.stop();
+	},
+	runs: count * 100,
+});
+
+runBenchmarks();
